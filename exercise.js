a9ca62c8fb6ce291b4e12b137d21cf7aed4ca9a6
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-20 02:55:44 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,116 @@
+// Create a function called uncompletedNotes that returns only not completed todos.
+
+// function uncompletedNotes(notes) {
+//   // ...
+// }
+
+// const notes = [
+//   {
+//     id: 1,
+//     description: "Workout program",
+//     todos: [
+//       {
+//         id: 1,
+//         name: "Push ups - 10 x 3",
+//         done: false,
+//       },
+//       {
+//         id: 2,
+//         name: "Abdominals - 20 x 3",
+//         done: true,
+//       },
+//       {
+//         id: 3,
+//         name: "Tapis Roulant - 15min",
+//         done: true,
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     description: "Front-end Roadmap",
+//     todos: [
+//       {
+//         id: 1,
+//         name: "Learn HTML",
+//         done: true,
+//       },
+//       {
+//         id: 2,
+//         name: "Learn CSS",
+//         done: true,
+//       },
+//       {
+//         id: 3,
+//         name: "Learn JavaScript",
+//         done: true,
+//       },
+//       {
+//         id: 4,
+//         name: "Learn Angular",
+//         done: false,
+//       },
+//     ],
+//   },
+// ];
+
+// uncompletedNotes(notes);
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+function uncompletedNotes(todo) {
+  return todo.done === false;
+}
+
+// to loop through the parent array and target the childs.
+for (let note of notes) {
+  console.log(note.todos.filter(uncompletedNotes));
+}
